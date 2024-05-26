ef4d4b98cb7bd07b45d57245df44aeb39c7a4551
Mikeermz <mikee.rmz@gmail.com>
2024-05-25 21:27:59 -0600

    feat: remove localhost
    Signed-off-by: Mikeermz <mikee.rmz@gmail.com>

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 //TODO: mover a 10
 export async function fetchProducts(page = 1, size = 10) {
   const res = await fetch(
-    `http://localhost:8000/api/v1/product?page=${page}&size=${size}`
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/product?page=${page}&size=${size}`
   );
 
   if (!res.ok) {
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}`,
 });
 
 export default api;
