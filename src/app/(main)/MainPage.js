0298fcae35f56b3fd89784d8a76212af3fee3bab
alanmaranto <alanarostegui@hotmail.com>
2024-10-21 23:58:16 -0600

    feat: remove logic for main page

--- src/app/(main)/MainPage.js
+++ src/app/(main)/MainPage.js
@@ -1,14 +1,9 @@
 "use client";
 
-import { Loading } from "../../components/Loading";
-import { ErrorUI } from "../../components/Error";
 import BrandCarousel from "./BrandCarousel";
 import Products from "./Products";
 
-export const MainPage = ({ brands, products }) => {
-  if (!brands || !products) return <Loading />;
-  if (brands.length === 0 || products.length === 0) return <ErrorUI main />;
-
+export const MainPage = ({ brands = [], products }) => {
   return (
     <>
       <BrandCarousel brands={brands} />
