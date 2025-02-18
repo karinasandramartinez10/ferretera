9d089e3c7be14e98852bdc2e9743966afc708617
alanmaranto <alanarostegui@hotmail.com>
2025-02-18 01:35:28 -0600

    fix: 0.12

--- src/app/(main)/checkout/page.jsx
+++ src/app/(main)/checkout/page.jsx
@@ -1,8 +1,13 @@
 import { auth } from "../../../auth";
+import { OrderProvider } from "../../../context/order/OrderProvider";
 import CheckoutPage from "./CheckoutNewPage";
 
 export default async function Page() {
   const session = await auth();
 
-  return <CheckoutPage session={session} />;
+  return (
+    <OrderProvider>
+      <CheckoutPage session={session} />
+    </OrderProvider>
+  );
 }
