128c1bf715b414428fd882e9a5fb4f28bd0f9956
alanmaranto <alanarostegui@hotmail.com>
2025-02-18 01:06:49 -0600

    fix: 0.10

--- src/hooks/order/useOrder.js
+++ src/hooks/order/useOrder.js
@@ -1,4 +1,10 @@
 import { useContext } from "react";
 import OrderContext from "../../context/order/OrderContext";
 
-export const useOrder = () => useContext(OrderContext);
+export const useOrder = () => {
+  const context = useContext(OrderContext);
+  if (!context) {
+    throw new Error("❌ useOrder debe ser usado dentro de un OrderProvider");
+  }
+  return context;
+};
