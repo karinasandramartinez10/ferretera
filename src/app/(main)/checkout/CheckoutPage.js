171797e1cc0f98b4fcd51fef19aa59abfbf229b8
alanmaranto <alanarostegui@hotmail.com>
2025-02-17 22:59:15 -0600

    chore: add log for checkout

--- src/app/(main)/checkout/CheckoutPage.js
+++ src/app/(main)/checkout/CheckoutPage.js
@@ -27,6 +27,8 @@ const CheckoutPage = () => {
   const [loading, setLoading] = useState(false);
   const { data: session } = useSession();
 
+  console.log("CheckoutPage - Session Data:", session);
+
   const isAuthenticated = !!session?.user;
 
   const {
