4e658a869daac63bdbabd927613064df195f3fff
alanmaranto <alanarostegui@hotmail.com>
2025-03-28 15:12:39 -0600

    feat(quote-product-card): add brand and modify styles

--- src/app/(admin)/admin/quotes/[id]/QuoteProductCard.jsx
+++ src/app/(admin)/admin/quotes/[id]/QuoteProductCard.jsx
@@ -1,17 +1,18 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const QuoteProductCard = ({ product }) => (
-  <Card variant="outlined" key={product.id} sx={{ mb: 2 }}>
+  <Card variant="outlined" key={product.id}>
     <CardMedia
       component="img"
       image={product.Files && product.Files[0]?.path}
       alt={product.name}
-      sx={{ height: 140, objectFit: "contain" }}
+      sx={{ height: 140, objectFit: "contain", margin: "16px" }}
     />
     <CardContent>
       <Typography variant="subtitle1">{product.name}</Typography>
+      <Typography variant="body" color="primary.main">{product.brand.name}</Typography>
       <Typography variant="body2" color="text.secondary">
-        {product.code}
+        Codigo - {product.code}
       </Typography>
       <Typography>
         <strong>Cantidad:</strong> {product.QuoteProduct?.quantity}
