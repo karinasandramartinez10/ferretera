656f02733187e6ff7278d032fb0fc13a2ce3215e
alanmaranto <alanarostegui@hotmail.com>
2025-05-13 01:15:51 -0600

    fix: force dynamic

--- src/app/(admin)/admin/quotes/page.jsx
+++ src/app/(admin)/admin/quotes/page.jsx
@@ -1,4 +1,4 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 import { fetchQuotesServer } from "../../../../actions/quotes";
 import { auth } from "../../../../auth";
 import { ErrorUI } from "../../../../components/Error";
