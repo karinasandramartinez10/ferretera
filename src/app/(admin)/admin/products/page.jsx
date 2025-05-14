1727eb6dc77c5b08f54071f41cb7b928884d77ea
alanmaranto <alanarostegui@hotmail.com>
2025-05-13 18:55:18 -0600

    revert: CSR for products

--- src/app/(admin)/admin/products/page.jsx
+++ src/app/(admin)/admin/products/page.jsx
@@ -1,18 +1,5 @@
-export const dynamic = "force-dynamic";
-import { fetchAllProductsServer } from "../../../../actions/product";
-import { ErrorUI } from "../../../../components/Error";
 import Products from "./Products";
 
 export default async function ProductsPage() {
-  try {
-    const products = await fetchAllProductsServer(1, 10);
-    return <Products initialData={products} />;
-  } catch (error) {
-    return (
-      <ErrorUI
-        href="/admin/products"
-        message="No pudimos cargar los productos"
-      />
-    );
-  }
+  return <Products />;
 }
