171d246b6857e353b23719ce502a7ae419059efd
alanmaranto <alanarostegui@hotmail.com>
2025-11-03 01:26:08 -0600

    feat(checkout): integrate fiscal profile selection

---
+++ src/app/(main)/checkout/MessageSection.jsx
@@ -0,0 +1,35 @@
+import { Stack, Typography } from "@mui/material";
+import { Controller } from "react-hook-form";
+import { TextField } from "@mui/material";
+
+const MessageSection = ({ control }) => (
+  <Stack mt={2} gap={1}>
+    <Typography variant="h4">Checkout</Typography>
+    <Typography id="quote-modal-description">Agrega un mensaje</Typography>
+    <Controller
+      control={control}
+      name="message"
+      render={({ field, fieldState: { invalid, error } }) => (
+        <TextField
+          label=""
+          multiline
+          fullWidth
+          placeholder="Quisiera saber si..."
+          error={invalid}
+          helperText={error?.message && error.message}
+          variant="outlined"
+          rows={4}
+          inputProps={{
+            form: { autocomplete: "off" },
+          }}
+          InputProps={{
+            sx: { borderRadius: "8px", background: "#FFF" },
+          }}
+          {...field}
+        />
+      )}
+    />
+  </Stack>
+);
+
+export default MessageSection;
