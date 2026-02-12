bc0469595a92c8ee322d1de93a739f90984e5cb4
alanmaranto <alanarostegui@hotmail.com>
2026-02-12 03:43:29 -0600

    refactor: extract shared types to src/types
    Create centralized type definitions for filters (FilterOption,
    FilterOptions, SelectedFilters), products (Product, ProductGroup,
    GroupedResult) and favorites (FavoriteEntry).

---
+++ src/types/filters.ts
@@ -0,0 +1,29 @@
+export interface FilterOption {
+  id: number | string;
+  name: string;
+  count?: number;
+}
+
+export interface MeasureOption extends FilterOption {
+  abbreviation?: string;
+}
+
+export interface FilterOptions {
+  brands?: FilterOption[];
+  categories?: FilterOption[];
+  subcategories?: FilterOption[];
+  types?: FilterOption[];
+  models?: FilterOption[];
+  measures?: MeasureOption[];
+  designs?: FilterOption[];
+}
+
+export interface SelectedFilters {
+  brandIds?: (number | string)[];
+  categoryIds?: (number | string)[];
+  subcategoryIds?: (number | string)[];
+  typeIds?: (number | string)[];
+  modelIds?: (number | string)[];
+  measureIds?: (number | string)[];
+  designIds?: (number | string)[];
+}
