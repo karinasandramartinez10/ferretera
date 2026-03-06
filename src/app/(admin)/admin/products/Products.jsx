15eb0bff19fe3e62bad2e985bd9acf9da8b19591
alanmaranto <alanarostegui@hotmail.com>
2026-03-06 03:05:37 -0600

    feat: add 500 page size option to admin products table

--- src/app/(admin)/admin/products/Products.jsx
+++ src/app/(admin)/admin/products/Products.jsx
@@ -102,7 +102,7 @@ const ProductsPage = () => {
         paginationMode="server"
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
-        pageSizeOptions={[10, 25, 50, 100]}
+        pageSizeOptions={[10, 25, 50, 100, 500]}
         disableRowSelectionOnClick
         sx={{
           height: 900,
